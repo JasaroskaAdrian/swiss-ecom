464296521549c9aa7462bcccc19a4bd77ed2b194
Adrian Jasaroska <adrianjasaroska@gmail.com>
2025-09-08 22:59:11 +0200

    Added settings.json, run_sql.js to run the sql scripts that i choose, database.js to create a pool connection with my db, certain migration sql files like users / customers / categories / products. Changed the Docker Compose Port for the postgres db since it was already allocated and changed the index.html

---
+++ src/server/db/database.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import pg from 'pg'
+const { Pool, Client } = pg
+
+async function main(){
+    const pool = new Pool({
+        host: '127.0.0.1',
+        port: '15432:5432',
+        user: 'admin',
+        password: process.env.POSTGRES_PASS,
+        database: 'shop',
+        max: 10
+    })
+    try {
+        console.log('CWD:', process.cwd())
+        console.log('POSTGRES_PASS set?', typeof process.env.POSTGRES_PASS === 'string');
+        // The Database connected now asks a query to check if theres something wrong internally
+        const res = await pool.query("SELECT 'HelloWorld' as message")
+        console.log("Yay, connected to the Database! :)")
+        console.log(res.rows[0].message)
+    } catch (err) {
+        console.log("DB error:", err.code, err.message)
+    } finally {
+        await pool.end()
+    }
+}
+main()
---
+++ src/server/db/migrations/00001_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+    first_name VARCHAR2(20)
+   ,last_name VARCHAR2(20)
+   , 
+)
+    
