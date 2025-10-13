046df4f270310ce6921362eb9756d5109bb7c860
Adrian Jasaroska <adrianjasaroska@gmail.com>
2025-10-13 15:29:19 +0200

    Created an Export from database.js to run-sql.js, so i dont use redundant code

--- src/server/api/bin/run_sql.js
+++ src/server/api/bin/run_sql.js
@@ -1,31 +1,8 @@
 import 'dotenv/config'
-import pg from 'pg'
+import client from "../../db/database"
 import fs from 'node:fs/promises'
 
 async function runSql(){
-    /*
-    Client Connection Arguments
-    */ 
-    const client = new pg.Client({
-        host: '127.0.0.1',
-        port: 15432,
-        user: 'admin',
-        password: process.env.POSTGRES_PASS,
-        database: 'shop'
-    })  
-    try {
-    /*
-    Check Up on Parameters
-    */
-        console.log('CWD:', process.cwd())
-        if (typeof process.env.POSTGRES_PASS === 'string'){
-            console.log('POSTGRES_PASS set?', true);
-        } else {
-            console.log('POSTGRES_PASS set?', false);
-        }
-    } catch (err) {
-        console.log("DB error:", err.code, err.message)
-    }
     try {
     /*
     Check Up if File exists or is getting read
--- src/server/db/database.js
+++ src/server/db/database.js
@@ -1,27 +1,29 @@
 import 'dotenv/config'
 import pg from 'pg'
-const { Pool, Client } = pg
 
-async function main(){
-    const pool = new Pool({
+export async function main(){
+    /*
+    Client Connection Arguments
+    */ 
+    const client = new pg.Client({
         host: '127.0.0.1',
-        port: '15432',
+        port: 15432,
         user: 'admin',
         password: process.env.POSTGRES_PASS,
-        database: 'shop',
-        max: 10
-    })
+        database: 'shop'
+    })  
     try {
+    /*
+    Check Up on Parameters
+    */
         console.log('CWD:', process.cwd())
-        console.log('POSTGRES_PASS set?', typeof process.env.POSTGRES_PASS === 'string');
-        // The Database connected now asks a query to check if theres something wrong internally
-        const res = await pool.query("SELECT 'HelloWorld' as message")
-        console.log("Yay, connected to the Database! :)")
-        console.log(res.rows[0].message)
+        if (typeof process.env.POSTGRES_PASS === 'string'){
+            console.log('POSTGRES_PASS set?', true);
+        } else {
+            console.log('POSTGRES_PASS set?', false);
+        }
     } catch (err) {
         console.log("DB error:", err.code, err.message)
-    } finally {
-        await pool.end()
     }
 }
 main()
