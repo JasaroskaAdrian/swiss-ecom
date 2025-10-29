f103172975aeb73bf6378f2c2b2e1b9b93450978
Adrian Jasaroska <adrianjasaroska@gmail.com>
2025-10-29 21:24:27 +0100

    refactored the repo to redesign ui in React

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path'
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
 export default defineConfig({
-    root: 'src/frontend/html',
-    server: { open: true },
-    build: {
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
         outDir: '../../../dist',
         emptyOutDir: true
     },
-    resolve: {
-        alias:{
-            '/scripts': resolve(__dirname, 'src/frontend/scripts')
-        }
-    }
 })
