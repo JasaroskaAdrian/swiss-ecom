9d2601d3afc1a9d359269a6829fcf5930735f017
Adrian Jasaroska <adrianjasaroska@gmail.com>
2025-11-09 23:53:13 +0100

    Add React Router and Place the HomePage into its own HomePage.tsx and configure the .prettierrc

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,35 +1,12 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Footer from './components/Footer'
-import ProductCard from './components/ProductCard'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+)
 
 const App = () => {
-  return (
-    <>
-    <div className="min-h-screen bg-bg text-text flex flex-col">
-    <div className="mx-auto max-w-content px-4 sm:px-6 lg:px-8 flex-1">
-
-    <Navbar />
-
-    {/** Hero*/}
-    <Hero />
-
-    {/** Products grid (placeholder) */}
-    <main id="products" className="mt-6">
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {/** … product cards … */} 
-        <ProductCard pName="Alpine Jacket Wolfskin" pStock={ 5 } pPrice={ 14900 }/>
-        {/** duplicate as needed*/}
-      </div>
-    </main>
-
-  </div>
-
-  {/** FOOTER*/}
-    <Footer />
-    </div>
-    </>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
---
+++ src/frontend/src/pages/HomePage.tsx
@@ -0,0 +1,35 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Footer from '../components/Footer';
+import ProductCard from '../components/ProductCard';
+
+const HomePage = () => {
+  return (
+    <>
+    <div className="min-h-screen bg-bg text-text flex flex-col">
+    <div className="mx-auto max-w-content px-4 sm:px-6 lg:px-8 flex-1">
+
+    <Navbar />
+
+    {/** Hero*/}
+    <Hero />
+
+    {/** Products grid (placeholder) */}
+    <main id="products" className="mt-6">
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {/** … product cards … */} 
+        <ProductCard pName="Alpine Jacket Wolfskin" pStock={ 5 } pPrice={ 14900 }/>
+        {/** duplicate as needed*/}
+      </div>
+    </main>
+
+    </div>
+
+    {/** FOOTER*/}
+    <Footer />
+    </div>
+    </>
+  )
+}
+
+export default HomePage
